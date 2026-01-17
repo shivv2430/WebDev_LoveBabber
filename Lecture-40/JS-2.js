42114de553aff079dd1c1c579d87a14716fe4016
shivv2430 <2430.shivanipal@gmail.com>
2026-01-17 19:21:15 +0530

    Lec-40 JS-2

---
+++ Lecture-40/JS-2.js
@@ -0,0 +1,36 @@
+                                         //    var()
+ // var age=26;
+                                         // if()
+// if ( true) {
+//     console.log(age);
+// }
+
+// function solve ( ){
+//     var age=25;
+//     console.log(age);
+// }
+// solve();
+                                        //  let()
+// {
+// let a=10;
+// console.log(a);}
+
+// let a=18;
+// a=" shivi";
+                                        // const
+// const a = 28;
+// console.log(a);
+
+             // Num_Datatype
+// let marks=78;                 //defined marks//
+// marks="Shivi";                //string//
+// marks=true;                   //boolean//
+// marks=null;                   //null//
+// let marks=432457654654354765655321887665432309887667543221112224870988098087655432431;                   //Big Int//
+// console.log (marks);                    //print
+
+// let marks;                                  //UnDefined//
+// console.log(marks);
+
+marks=false;
+console.log(typeof(marks));
