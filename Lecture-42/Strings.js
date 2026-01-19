0c3b1c29f11de2aa09e495ed13ad2b539407a4d8
shivv2430 <2430.shivanipal@gmail.com>
2026-01-19 20:18:30 +0530

    Lec-42_Strings+Loops

---
+++ Lecture-42/Strings.js
@@ -0,0 +1,48 @@
+
+// let fname="shivani";
+// let lname="pal";
+// let Name=`hello
+// its 
+// shivani
+// pal`;
+// console.log(Name);
+
+// let firstName = new String("Love Babbar");
+// console.log(firstName);
+
+//                                        //  Operations//
+//               // Concatenation//
+// let firstname = "Shivani";
+// let Myname = "SHIVANI";
+// let lastname = "Pal";
+// // let Name = firstname+ " "+lastname;
+// let Name=`$(firstname)$(lastname)`;
+// console.log(Name);
+
+//                 // length //
+// console.log(firstname.length);
+
+                // uppercase //
+// console.log(firstname.toUpperCase());
+
+                //lowercase//
+// console.log(Myname.toLowerCase());
+
+                //substring//
+// let str = "Babber";
+// console.log(str.substring[2,4]);
+
+                // split method
+let sentence = "hello ji kaise ho sare";
+// let sent = "hello ji \"kaise\" ho sare";
+// let tence = "hello \\ji \\kaise\\ho\\ sare";
+
+let words = sentence.split(" ");
+// let word = sent.split(" ");
+// let ward = tense.split(" \\");
+
+
+console.log(words);
+// console.log(word);
+// console.log(ward);
+console.log(words.join('-'));
