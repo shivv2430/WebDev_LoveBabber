53cda85d46a6a7949b6fcf7c8e8df8b32c7eb860
shivv2430 <2430.shivanipal@gmail.com>
2026-01-19 19:11:38 +0530

    Loops

---
+++ Lecture-42/Loops.js
@@ -0,0 +1,53 @@
+//                                             // FOR LOOP//
+// // print shivi 10 times
+// for(let i=5;i>=1;i--){
+//     console.log("Shivi");
+// }
+
+// // counting
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// // Reverse Counting
+// for(let i=5;i>=1;i--){
+//     console.log(i);
+// }
+
+// // Break
+// for(let i=1;i<=5;i++){
+//     if(i==4)
+//         break;
+//     else console.log(i);
+// }
+
+// // Continue
+// for (let i=1;i<=5;i++){
+//     if(i==3)
+//         continue;
+//     else console.log(i);
+// }
+                                            // WHILE LOOP
+// let j=1;
+// while(j <=5 ){
+//     console.log("hello");
+//     j++;
+// }
+
+// let i=1;
+// while(i<=5){
+//     if(i==4){
+//       i++;
+//         continue;
+//     }
+//     else 
+//          console.log(i);
+//     i++;
+// }
+                                            // DO WHILE LOOP
+let i=10;
+do{
+    console.log(i);
+    i--;
+}
+while(i>=1);
