85c464f13d3a78df59f359ecbe54f9357ece65bb
shivv2430 <2430.shivanipal@gmail.com>
2026-01-20 16:41:47 +0530

    change name & add Lec-44

---
+++ Lecture-44/Arrays.js
@@ -0,0 +1,12 @@
+let obj= {
+    Name:"shivani",
+    age:"19",
+    weight:"35Kg" ,
+    height:"4ft 7inch",
+    greet : function(){
+        console.log("hello jii kaise ho sare");
+    }
+};
+console.log(obj);
+obj.greet();
+console.log(typeof(obj));
