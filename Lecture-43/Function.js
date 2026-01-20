269b3febb527346d99777b1f03e0c5afadcfe8c2
shivv2430 <2430.shivanipal@gmail.com>
2026-01-20 14:56:51 +0530

    Js-5_functions

---
+++ Lecture-43/Function.js
@@ -0,0 +1,65 @@
+//         // Function Definition//
+// function saymyname(){
+//     console.log("Shivani pal");
+// }
+
+//         // Function call//
+// saymyname();
+
+// //   PrintCounting //
+// function PrintCounting(){
+//     for(let i=0;i<4;i++)
+//         console.log(i);
+// }
+
+// // PrintCounting();
+// function PrintNumber(num){
+//     console.log("Print Number : ",num);
+// }
+
+// // PrintNumber(5);
+// function getAverage(num1,num2){
+//         let Avg=(num1+num2)/2;      //average formula used
+//          console.log("Average : ",Avg);   
+// }
+// getAverage(5,3);                 // Actual number
+
+//                                 // RETURN FUNCTION
+// function getSum(n1,n2,n3){
+// let sum=n1+n2+n3;
+// return sum;}
+
+// let ans=getSum(1,2,3);
+// console.log("Sum : ",ans);
+
+// function getmyname(firstname,lastname){
+//     let fullname=firstname + " " +lastname;
+//     return fullname;
+//     //unreachable code
+// }
+
+// let Name=getmyname("shivani","pal");
+// console.log("my name : ", Name);
+
+// function getMultiplication(a,b){
+//     return a*b;
+// }
+// console.log(getMultiplication(2,10));
+
+// function square(num){
+//    let sq=num*num;
+//    return sq;
+// }
+// let Sq=square(4);
+// console.log("square of number : ",Sq);
+
+
+   // Arrow function//
+function getExp(x,y) // =>// 
+{
+    let ans = x**y;
+    return ans;
+}
+console.log( getExp(2,3));
+ 
+
