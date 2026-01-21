e02c85bf42be7a8f7b05f5a36205616584350b93
shivv2430 <2430.shivanipal@gmail.com>
2026-01-21 20:30:22 +0530

    update_Arrays

--- Lecture-44/Arrays.js
+++ Lecture-44/Arrays.js
@@ -30,9 +30,123 @@
 // // console.log(typeof(arr));
 // // console.log(typeof(brr));
 
-let arr=[n1,n2,n3];
-let ansArray = arr.map((number){
-    return number*number;
-})
 
-console.log(ansArray);
+// let arr=[10,20,30];
+// arr.map((number) =>{
+//     console.log(number+1);
+// })
+// arr.map((number,index) => {
+//     console.log(number,index);
+//     // console.log(index);
+// })
+                                // Filter
+// let arr =[10,20,30,11,21,44,51];
+// let evenArray = arr.filter((number) => {
+//     return number%2 ===0;});
+//     if ( number%2 === 0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// })
+// console.log(evenArray)
+
+
+// let ansArray = arr.map((number) =>{
+//     return number*number;
+// });
+
+// console.log(ansArray);
+
+// let arr =[1,2,'Kunal','babbar',null];
+// let ans =arr.filter((value) => {
+//     if( typeof(value) === 'string') {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// });
+// console.log( ans);
+
+
+// let arr=[10,20,30,40];
+// let ans =arr.reduce((acc,curr) => {
+//     return acc + curr;
+// },0);
+// console .log(ans);
+
+//             // Ascending order
+// let arr = [9,1,7,4,2,8];
+// arr.sort();        //acsending order
+
+// // arr.sort((a,b) => b-a);             //descending order
+
+// console.log(arr);
+
+// console.log(arr.indexOf(9));           //indexing of any value
+
+                                     //FOR EACH 
+// let arr = [10,20,30];
+// let length = arr.length;
+// console.log("length: " , length);
+// // tradinational method
+// for( let index=0;index<length;index++){
+//     console.log (arr[index]);
+
+// }
+
+// arr.forEach((value,index) =>{
+//     console.log(" Number : ", value ,"Index : ",index);
+// })
+
+                                         // FOR IN
+// let obj= {
+//     Name:"shivani",
+//     age:"19",
+//     weight:"35Kg" ,
+//     height:"4ft 7inch",
+//     greet : function(){
+//         console.log("hello jii kaise ho sare");
+//     }
+// };
+// for (let key in obj){
+//     console.log(key," ",obj[key]);
+// }
+// console.log(obj);
+// // obj.greet();
+// // console.log(typeof(obj));
+
+                                //FOR-OF
+// let arr = [10,20,30,40];
+// for(let value of arr){
+//     console.log (value);
+// }
+// let fullName ="Shivani";
+// for(let val of fullName){
+//     console.log (val);
+// }
+
+                            // Arrays  with function
+let arr =[10,20,30,40,50];
+
+// function getSum(arr) {
+
+let getSum = (arr) => {
+    arr.forEach((value) =>{
+        let sum=0;
+        arr.forEach((value) =>{
+             sum = sum + value;
+        })
+       return sum;
+    })
+    // let len = arr.length;
+    // let sum = 0;
+    // for(let index=0; index<len; index++){
+    //     sum = sum + arr[index];
+    // }
+    // return sum;
+}
+let totalSum = getSum(arr);
+console.log(totalSum);
