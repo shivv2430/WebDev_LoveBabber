e5773fe60f545bfaba0504f7b8fc89e9e7c16049
shivv2430 <2430.shivanipal@gmail.com>
2026-01-24 21:03:47 +0530

    Object-type

---
+++ Lecture-48/DateObjects.js
@@ -0,0 +1,28 @@
+// their are also many more date functions
+let curr=new Date();
+let date=new Date('july 30 2006 6:20');
+let dob=new Date(' 2006,6,30, 6:20');
+
+console.log(date);
+console.log(curr);
+console.log(dob);
+
+console.log(newDate.getDay());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lecture-48/MathObjects.js
@@ -0,0 +1,46 @@
+// Objects
+
+console.log("Shivani");
+
+console.log(Math.PI);           //3.14159.....
+
+console.log(Math.max(60,30,24,600,712,893));        //893
+
+console.log(Math.min(60,30,24,600,712,893));        //24
+
+console.log(Math.round(1.3));               //1
+console.log(Math.round(1.7));               //2
+
+console.log(Math.floor(1.7));               //1
+console.log(Math.ceil(1.1));               //2
+
+console.log(Math.abs(-7));               //7
+
+console.log(Math.random());               //give random  value
+
+console.log(Math.sqrt(5));               //25
+
+console.log(Math.pow(2,3));               //2
+
+let value = Math.E;     //Euler's number
+console.log(value);         //Output : 2.718281828459045
+
+let LN2 = Math.LN2;
+console.log(LN2);         //Output : 0.6931471805599453
+
+let LN10 = Math.LN10;
+console.log(LN10);          //Output : 2.302585092994046
+
+let LOG2E = Math.LOG2E;
+console.log(LOG2E);            //Output : 1.4426950408889634
+
+let LOG10E = Math.LOG10E;
+console.log(LOG10E);        //Output : 0.4342944819032518
+
+
+
+
+
+
+
+
