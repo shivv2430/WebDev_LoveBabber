bcd7fc22eb735ae47ba4283f9b8f075d54c62340
shivv2430 <2430.shivanipal@gmail.com>
2026-02-10 20:40:24 +0530

    editing events code

--- JAVASCRIPT/Lecture-53/Events.js
+++ JAVASCRIPT/Lecture-53/Events.js
@@ -4,7 +4,8 @@
 //event listener.  "video play(action)"
 //<event-target>.addEventListener(<event-type>,<function->action>)
 
-function changeText(){
+function changeText(event){
+    console.log(event);
     let fpara= document.getElementById('fpara');
     fpara.textContent = "Hello Shivani"
 }
@@ -16,4 +17,30 @@ fpara.removeEventListener('click',changeText);
                                         //Phase of Event
 //Caputuring phase(upar se niche div mei tree ki trha){true->usecapture}
 //Bubbling phase(niche se upar {At-Target}){add event listener ->Bubbling phase}
-//EventObject
+
+//EventObject.   adding console.log(event) in function changeText()
+
+                                        //Default Action
+
+// let anchorElement = document.getElementById('fanchor');
+// anchorElement.addEventListener('click',function (event){
+//     event.preventDefault();
+//     anchorElement.textContent = "click done bhyi"
+// });
+
+// let paras = document.querySelectorAll('p');
+function alertPara (event){
+    if(event.target.nodeName == 'SPAN'){
+     alert("You have clicked on para: "+ event.target.textContent)
+}
+}
+// for(let i=0;i<paras.length;i++){
+//     let para= paras[i];
+//     para.addEventListener("click",function (){
+//          alert("You have clicked on para: "+(i+1));
+//     })
+   
+// }
+let mydiv =document.getElementById('wrapper');
+document.addEventListener('click',alertpara);
+
