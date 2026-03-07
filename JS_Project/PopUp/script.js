d80323de9c600798ac707dd770e15194badc23f2
shivv2430 <2430.shivanipal@gmail.com>
2026-03-07 19:41:57 +0530

    update in popup

--- JS_Project/PopUp/script.js
+++ JS_Project/PopUp/script.js
@@ -1,8 +1,9 @@
 let popup = document.getElementById("popup");
 
 function openPopup(){
-     popup.classList.add(".open-popup");
+     popup.classList.add("open-popup");
 }
+
 function closePopup(){
-     popup.classList.remove(".open-popup");
+     popup.classList.remove("open-popup");
 }
