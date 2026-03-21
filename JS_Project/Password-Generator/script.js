128a12f06698e4a6710665be9c2a670286607f2f
shivv2430 <2430.shivanipal@gmail.com>
2026-03-21 21:59:11 +0530

    u

--- JS_Project/Password-Generator/script.js
+++ JS_Project/Password-Generator/script.js
@@ -1,14 +1,18 @@
+let notifications = document.getElementById('notifications');
+let generate_password = document.getElementById('generate_password');
+let copyBtn = document.getElementById('copyPassword');
+
 const passwordBox = document.getElementById("password");
 const length = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789"
-const symbol = "!@#$%^&*()><?:{}[]\|~``"
+const symbol = "!@#$%^&*()><?:{}[]\\|~``"
 const allChars = upperCase + lowerCase + number +symbol;
 
 function createPassword(){
-    let password = " ";
+    let password = "";
     password +=upperCase[Math.floor(Math.random() * upperCase.length)];
     password +=lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password +=number[Math.floor(Math.random() * number.length)];
@@ -23,7 +27,36 @@ function createPassword(){
 
 function copyPassword(){
     passwordBox.select();
-    document.execCommand("copy");
+    navigator.clipboard.writeText(passwordBox.value);
     
 
+}
+
+function createToast(type,icon,title,text){
+    let NewToast = document.createElement('div');
+    NewToast.innerHTML = `
+    <div class="toast ${type}">
+            <i class="${icon}"></i>
+            <div class="content">
+                <div class="title">${title}</div>
+                <span>${text}</span>
+            </div>
+            <i class="ri-checkbox-circle-fill" onclick="this.parentElement.remove()"></i>
+        </div>`;
+        notifications.appendChild(NewToast);
+        NewToast.timeOut = setTimeout(
+            ()=>NewToast.remove(),5000
+        )
+}
+
+generate_password.onclick = function(){
+    let type = 'generate_password';
+    let icon = 'ri-checkbox-circle-fill';
+    let title = 'generate_password';
+    let text = 'New password Generate';
+    createToast(type,icon,title,text);
+}
+copyBtn.onclick = function (){
+    copyPassword();
+    createToast('copy','ri-file-copy-fill','Copied','Password copied');
 }
