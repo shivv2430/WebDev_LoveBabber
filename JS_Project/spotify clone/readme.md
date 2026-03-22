8e8ce710204e968cac85d1d022c74e159d928667
shivv2430 <2430.shivanipal@gmail.com>
2026-03-22 12:23:32 +0530

    Add spotify clone project and .gitignore, set user email

---
+++ JS_Project/spotify clone/readme.md
@@ -0,0 +1,9 @@
+# Spotify Clone
+
+Starter files:
+
+- `index.html`
+- `style.css`
+- `script.js`
+
+You can build your Spotify-inspired music player UI from here.
---
+++ JS_Project/spotify clone/script.js
@@ -0,0 +1,5 @@
+const playButton = document.querySelector(".play-btn");
+
+playButton.addEventListener("click", () => {
+  alert("Your Spotify Clone starter is ready!");
+});
