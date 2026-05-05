e9d22d4239ba05a2f4e6457dbe8b7249d5e0cb3f
shivv2430 <2430.shivanipal@gmail.com>
2026-05-05 07:08:27 +0530

    add js in portfolio

---
+++ PROJECT/Portfolio/script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("Message Sent Successfully!");
+}
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
