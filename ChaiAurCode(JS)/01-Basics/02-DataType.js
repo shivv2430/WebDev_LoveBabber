490fc6945a4eca954e6a04f5cc920830ec3767f7
shivv2430 <2430.shivanipal@gmail.com>
2026-07-01 13:37:04 +0530

    dataType in JS

---
+++ ChaiAurCode(JS)/01-Basics/02-DataType.js
@@ -0,0 +1,26 @@
+"use strict"  //treat all JS cose a s a newer version
+//"use strict" automatically enables strict mode in the entire script.
+
+// number => 2 to power 53
+// bigint => 2 to power 53+1
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique value
+
+// alert(3 + 3)
+
+console.log(typeof 3 + 3)
+
+//Object
+console.log(typeof null)
+
+let name = "Shivani"   //string
+let age = 21           //number
+let isLoggedIn = false //boolean
+let out = null         //null
+let something          //undefined
+let value = 4543219007199254740991n  //bigint
+let id = Symbol("1")  //symbol
+
