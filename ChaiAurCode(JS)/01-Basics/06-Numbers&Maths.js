41c82d39d3fa2152fafa65770169c63a92b73163
shivv2430 <2430.shivanipal@gmail.com>
2026-07-03 19:33:58 +0530

    numbers and Maths

---
+++ ChaiAurCode(JS)/01-Basics/06-Numbers&Maths.js
@@ -0,0 +1,40 @@
+const score = 400;
+
+const balance = new Number(100.33434);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.89876;
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Number.MAX_VALUE);
+
+
+//+++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.PI);
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 2, 3, 4, 5, 6, 7, 8, 9, 0));
+console.log(Math.max(4, 2, 3, 4, 5, 6, 7, 8, 9, 0));
+
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
