5736142b583daaf5a864f19a828ccbc63da5af58
shivv2430 <2430.shivanipal@gmail.com>
2026-07-08 16:24:26 +0530

    Functions part-2 in js

--- ChaiAurCode(JS)/03-Basics/01-Functions.js
+++ ChaiAurCode(JS)/03-Basics/01-Functions.js
@@ -49,4 +49,40 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage("shivani"));
-console.log(loginUserMessage());   //undefined because nothing is passed in the argument and if not passed in function return undefined (and it is not error)
+// console.log(loginUserMessage());   //undefined because nothing is passed in the argument and if not passed in function return undefined (and it is not error)
+
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000)) //output --> [ 500, 2000 ] (rest operator)
+
+
+//object in function
+
+const user = {              //user is object
+    username: "shivi",
+    price: 199.       //if there were two key with same name like price and prices it will only take first key name
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+
+// handleObject(user);
+handleObject({ username: "sammy", price: 299 })
+
+
+//array in function
+
+const myArray = [200, 400, 100, 600]
+
+function handleArray(anyarray) {
+    console.log(`array element is ${anyarray[0]} and ${anyarray[1]}`);
+}
+
+handleArray(myArray);
+
+
