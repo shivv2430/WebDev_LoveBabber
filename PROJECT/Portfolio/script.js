c4042eecef423c7f2b2e00777f4254e004b485df
shivv2430 <2430.shivanipal@gmail.com>
2026-07-09 18:42:43 +0530

    remove duplicate project

--- PROJECT/Portfolio/script.js
+++
@@ -1,9 +0,0 @@
-function showAlert() {
-    alert("Message Sent Successfully!");
-}
-
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-        behavior: "smooth"
-    });
-}
