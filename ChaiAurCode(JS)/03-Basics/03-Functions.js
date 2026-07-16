dd694da5b3654f1e567bbb60c7c8526cd8636b67
shivv2430 <2430.shivanipal@gmail.com>
2026-07-16 19:19:12 +0530

    arrow-function in js

--- ChaiAurCode(JS)/03-Basics/03-Functions.js
+++ ChaiAurCode(JS)/03-Basics/03-Functions.js
@@ -28,3 +28,27 @@ const chai2 = function () {
 }
 chai2();
 
+// Arrow Function
+const chai3 = () => {
+    console.log(this);   // return global object -> window in browser -- it's different in node 
+    let username = "Sam";
+    console.log(this.username);   // Undefined in node -- 
+}
+chai3();
+
+//basic arrow function
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+console.log(add(2, 3));
+
+//Implicit Return
+const addTwo = (num1, num2) => num1 + num2;
+console.log(addTwo(3, 5));
+
+const addThree = (num1, num2, num3) => ({ username: "shivi" }); // return object
+
+console.log(addThree(3, 5, 7));
+
+const myArray = [2, 4, 5, 6, 3, 8];
+myArray.forEach();
