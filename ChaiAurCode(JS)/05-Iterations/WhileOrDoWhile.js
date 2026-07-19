a94dee3cb413fa69e89bb64136c9ffcdc9fcbcf3
shivv2430 <2430.shivanipal@gmail.com>
2026-07-19 13:06:55 +0530

    while or do-while loop in JS

---
+++ ChaiAurCode(JS)/05-Iterations/WhileOrDoWhile.js
@@ -0,0 +1,20 @@
+
+let index = 0;
+while (index <= 10) {
+    console.log(`Value of index is ${index}`)
+    index = index + 2;
+}
+
+let myArr = ["flash", "batman", "superman"];
+let arr = 0;
+while (arr < myArr.length) {
+    console.log(`Value is : ${myArr[arr]}`);
+    arr++;
+}
+
+
+let score = 1;
+do {
+    console.log(`Score is ${score}`);
+    score = score + 2;
+} while (score <= 10);
