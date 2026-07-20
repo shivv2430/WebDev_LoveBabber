7aefced18e7b909dfbed5181d97f43395b7f9d53
shivv2430 <2430.shivanipal@gmail.com>
2026-07-20 18:42:10 +0530

    maps in JS

--- ChaiAurCode(JS)/05-Iterations/High-OrderLoop.js
+++ ChaiAurCode(JS)/05-Iterations/High-OrderLoop.js
@@ -7,3 +7,26 @@ const arr = [1, 2, 3, 4, 5, 6];
 for (const num of arr) {
     console.log(num);
 }
+
+//Maps --> for unique value and order is maintained
+const map = new Map();
+map.set('IN', "India");
+map.set('US', "United States");
+map.set('FR', "France");
+map.set('IN', "India");//this will not be printed as map keys are unique
+
+for (const [key, value] of map) {
+    console.log(key, '->', value);
+}
+
+//Objects are not iterable in for of loop
+
+const myObj = {
+    game1: "NFS",
+    game2: "GTA",
+    game3: "Valorant"
+};
+
+for (const [key, value] of myObj) {
+    console.log(key, '->', value);
+}
